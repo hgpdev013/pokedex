d7740420040ef8b7a56a8eddbe01561371ea1f4e
Hugo Henrique <hugohenrique7@hotmail.com.br>
2023-01-24 22:40:49 -0300

    fixed pokemon type dimension bug

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,41 +2,41 @@ import React from 'react'
 import loading from "./assets/loading.gif"
 
 
-export const Pokemon = ({dados, image}) => {
-  const pokemonType = dados.types.filter(d => d.type).map(p => p.type.name)
+export const Pokemon = ({ dados, image }) => {
+  const pokemonType = dados.types.filter(d => d.type).map(p => p.type.name);
 
+  const colours = {
+    rock: '#B69E31',
+    ghost: '#70559B',
+    steel: '#B7B9D0',
+    water: '#6493EB',
+    grass: '#74CB48',
+    psychic: '#FB5584',
+    ice: '#9AD6DF',
+    dark: '#75574C',
+    fairy: '#E69EAC',
+    normal: '#AAA67F',
+    fighting: '#C12239',
+    flying: '#A891EC',
+    poison: '#A43E9E',
+    ground: '#DEC16B',
+    bug: '#A7B723',
+    fire: '#F57D31',
+    electric: '#F9CF30',
+    dragon: '#7037FF'
+  }
 
   return (
     <div className='pokemonFrame'>
-        <p className='pokemonName'>{dados.name}</p>
-        {!image ? <img src={loading} className="loadingGif"/> 
-        : <img src={image} alt="" className='pokemonPhoto'/>}
-        <ul className='typeFather'>
-          {pokemonType.map(
-            t => <li className={'type'} key={t} style={
-              {backgroundColor: t === 'rock' ? '#B69E31' : 
-                (t === "ghost" ? '#70559B' : 
-                (t === "steel" ? '#B7B9D0' : 
-                (t === "water" ? '#6493EB' :
-                (t === "grass" ? '#74CB48' :
-                (t === "psychic" ? '#FB5584' :
-                (t === "ice" ? '#9AD6DF' :
-                (t === "dark" ? '#75574C' :
-                (t === "fairy" ? '#E69EAC' :
-                (t === "normal" ? '#AAA67F' :
-                (t === "fighting" ? '#C12239' :
-                (t === "flying" ? '#A891EC' :
-                (t === "poison" ? '#A43E9E' :
-                (t === "ground" ? '#DEC16B' :
-                (t === "bug" ? '#A7B723' :
-                (t === "fire" ? '#F57D31' :
-                (t === "electric" ? '#F9CF30' : '#7037FF')))))))))))))))),
-                padding: '10px 10px',
-                borderRadius: '20px'
-              }
-            }> {t} </li>
-          )}
-        </ul>
+      <p className='pokemonName'>{dados.name}</p>
+      {!image ? <img src={loading} className="loadingGif" />
+        : <img src={image} alt="" className='pokemonPhoto' />}
+
+      <ul className='typeFather' style={{ justifyContent: pokemonType.length <= 1 ? 'center' : 'space-between' }}>
+        {pokemonType.map(t => <li className='type' key={t} style={
+          { backgroundColor: colours[t], padding: '10px 10px', borderRadius: '20px' }
+        }>{t}</li>)}
+      </ul>
     </div>
   )
 
